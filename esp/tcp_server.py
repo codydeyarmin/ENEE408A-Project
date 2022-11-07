9736e332d87017f6ef696ce5f3d7aa91195e23be
Gary <sneed.garrett@gmail.com>
2022-11-06 22:57:02 -0500

    latest updates to ESP setup
    including msgpack

--- esp/tcp_server.py
+++ esp/tcp_server.py
@@ -1,11 +1,39 @@
 import socket
+import msgpack
+import random
+from string import ascii_lowercase
 
-socket.setdefaulttimeout(30)
+#socket.setdefaulttimeout(30)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #srv_addr = ('172.24.44.109', 40810)
 srv_addr = ('192.168.1.3', 40810)
 sock.bind(srv_addr)
 sock.listen()
+
+byteseq = b'408A'
+
+def randomizeCommands():
+    randArr = [random.randint(0,255) for _ in range(3)] + [''.join([random.choice(ascii_lowercase) for _ in range(10)])]
+    bites = msgpack.packb(randArr, use_bin_type=False)
+    return bites, randArr
+
+def calcFirstByte(data):
+    newdata = b''
+    ctr = 0
+    idx = 0
+    while (idx < len(data)-1):
+        if data[idx] in [0xcc, 0xd0, 0xd9, 0xc4, 0xc7]: # 8-bits
+            newdata += data[idx:idx+2]
+            ctr += 1
+            idx += 2
+        elif data[idx] in [0xc5, 0xc8, 0xcd, 0xd1, 0xda]: # 16-bits
+            newdata += data[idx:idx+3]
+            ctr += 1
+            idx += 3
+        else: # other
+            idx += 1
+    
+    return bytes([0x90 + ctr]), newdata
 
 while True:
     print("Waiting for connection")
@@ -14,17 +42,22 @@ while True:
 
     try:
         print("Connected to client IP: {}".format(client))
-
+        
         # Receive and print data 32 bytes at a time, as long as the client is sending something
         while True:
-            data = connection.recv(32)
-            print("Received data: {}".format(data))
+            # RECEIVE
+            data = connection.recv(1024)
+            if data:
+                b, d = calcFirstByte(data[:-4])
+                print("Received data: {}".format(data))
+                print("unpacking {}".format(b + d))
+                unpacked = msgpack.unpackb(b + d, raw=True)
+                print("Unpacked data: {}".format(unpacked))
 
-            if not data:
-                break
-
-            print(f'sending back to ESP')
-            connection.send(b'hello back from Gary\'s computer ;)\n');
+            # SEND
+            ### bites, randArr = randomizeCommands()
+            ### print(f'sending data to ESP ---> {randArr}')
+            ### connection.sendall(bites + b'\n');
 
     finally:
         connection.close()
--- esp/test_arduino/test_arduino.ino
+++ esp/test_arduino/test_arduino.ino
@@ -1,56 +1,125 @@
 #include "Arduino.h"
 #include "SoftwareSerial.h"
 #include "ArduinoJson.h"
+#include "MsgPack.h"  
 
-int tx = 11;
-int rx = 10;
-SoftwareSerial* mSerial = new SoftwareSerial(rx, tx); //connects to wifi module
-//DynamicJsonDocument doc(1024);
+int tx = 11, rx = 10;
+SoftwareSerial mSerial(rx, tx); //connects to wifi module
+
+int aX, aY, aZ, gX, gY, gZ;
+float temp;
+
+int dX, dY, dZ;
+String stuff;
+
 String receiving, sending;
+bool initial = false;
+byte flushSeq[4] = {0x34, 0x30, 0x38, 0x41}; // 408A
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  pinMode(rx, INPUT);
-  pinMode(tx, OUTPUT);
-  
-  mSerial->begin(57600); 
-  delay(5000);
+// Generate random values to send to server as test
+void generateTelemetry() {
+  aX = random(-1024, 1024);
+  aY = random(-1024, 1024);
+  aZ = random(-1024, 1024);
+  gX = random(-1024, 1024);
+  gY = random(-1024, 1024);
+  gZ = random(-1024, 1024);
+  temp = (float) (random(0, 1024) / 5);
 }
 
-int receive() {
+// RECEIVE
+int receiveData() {
+  Serial.println("idk trying to receive");
+  delay(500);
   receiving = "";
-  while (mSerial->available()) {
-    char c = (char) mSerial->read();
+  byte test[256];
+  int idx = 0;
+  
+  while (mSerial.available() > 0) {
+    char c = (char) mSerial.read();
+    test[idx++] = (byte) c;
     receiving += c;
+    
     //Serial.print((byte)c);
     //Serial.print(" ");
   }
-  Serial.println("");
-  if (receiving.length() > 0) {
-    Serial.print("Received: ");
-    Serial.println(receiving);  
-  }
 
+  delay(500);
+  Serial.println("check length");
+  delay(500);
+  if (receiving.length() > 0) {
+    Serial.print("Received bytes: ");
+    Serial.println(receiving); 
+
+    if (initial) {
+      MsgPack::Unpacker unpacker;
+      unpacker.feed(test, idx);
+      unpacker.deserialize(dX, dY, dZ, stuff);
+      
+      Serial.print("Received data: ");
+      Serial.print(dX);
+      Serial.print(", ");
+      Serial.print(dY);
+      Serial.print(", ");
+      Serial.print(dZ);
+      Serial.print(", ");
+      Serial.println(stuff);
+    }
+  }
+  
+  delay(500);
   //Serial.print((receiving != "connection established"));
   return (receiving != "connection established\r\n");
 }
 
-void sendTo() {
-  Serial.println("Sending message to TCP server from Arduino");
+// SEND
+void sendData() {
+  MsgPack::Packer packer;
+  generateTelemetry();
+  //int first = 1024;
+  packer.serialize(aX, aY, aZ, gX, gY, gZ);
+  
+  Serial.println("Sending telemetry data: 256, ");
+  Serial.print(aX);
+  Serial.print(", ");
+  Serial.print(aY);
+  Serial.print(", ");
+  Serial.print(aZ);
+  Serial.print(", ");
+  Serial.print(gX);
+  Serial.print(", ");
+  Serial.print(gY);
+  Serial.print(", ");
+  Serial.print(gZ);
+  Serial.print(" ---- ");
+  Serial.write((char *) packer.data());
+  Serial.println(" ");
+  
+  
+  delay(500);
   sending = "";
-  mSerial->println("Hello from Arduino Uno!!");
+  //mSerial->write("Hello from Arduino Uno!!");
+  mSerial.write((char *)packer.data());
+  mSerial.write((char *) flushSeq);
+}
+
+void setup() {
+  pinMode(rx, INPUT);
+  pinMode(tx, OUTPUT);
+  
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  mSerial.begin(9600); 
+  Serial.println("begin");
+  //delay(500);
+  //while (receiveData());
+  //initial = true;
+  //Serial.println("!!! received connection established !!!");
+  delay(1000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-  while (receive());
-  delay(1000);
-  Serial.println("!!! received connection established !!!");
-  sendTo();
-  delay(1000);
-  receive();
-  
-  delay(300000);
+  sendData();
+  receiveData();
+  Serial.println("end loop");
 }
