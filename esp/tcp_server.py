c7e9fd770fbe91a47054877d86db47f9f91e5963
Gary <sneed.garrett@gmail.com>
2022-12-08 22:07:46 -0500

    newest tcp_server.py
    just edits

--- esp/tcp_server.py
+++ esp/tcp_server.py
@@ -8,16 +8,16 @@ import time
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 #srv_addr = ('172.24.44.109', 40810)
-#srv_addr = ('192.168.1.4', 40812)
+addr = ('192.168.4.1', 40810)
 #srv_addr = ('127.0.0.1', 40812)
-addr = ('192.168.1.3', 40810)
+#addr = ('192.168.1.3', 40810)
 #sock.bind(srv_addr)
 #sock.listen()
 
 byteseq = b'408A'
 
 def randomizeCommands():
-    randArr = [random.randint(0,1024) for _ in range(5)] # + [''.join([random.choice(ascii_lowercase) for _ in range(10)])]
+    randArr = [random.randint(0,1024) for _ in range(6)] # + [''.join([random.choice(ascii_lowercase) for _ in range(10)])]
     bites = msgpack.packb(randArr, use_bin_type=True)
     return bites, randArr
 
@@ -60,30 +60,37 @@ while True:
         while True:# and (time.time() - start < 10):
             # SEND
             bites, randArr = randomizeCommands()
-            print(f'\nsending data to ESP ---> {randArr}')
-            sock.sendall(bites)
+            #sock.sendall(bites + b'408A')
+            sendthis = bites + b'408A' + (30 - len(bites) - 4)*b'\x01'
+            print(f'\nsending data to ESP ---> {sendthis}, length = {len(sendthis)}')
+            print(f'sent: {randArr}')
+            sock.sendall(sendthis)
             #sendit = False
             #continue
             
             # RECEIVE
-            data += sock.recv(1024)
+            #data += sock.recv(1024)
+            
+            while (b'408A' not in data):
+                data += sock.recv(1)
+                #print(data)
             #print(f'data = {data}')
-            if len(data) > 4 and data[-4:] == b'408A':
-                data = b'\x96' + data.split(b'\x96')[-1]
-                #b, d = calcFirstByte(data[:-4])
-                #print("Received data: {}".format(data))
-                #print("unpacking {}".format(b + d))
-                #print("unpacking {}".format(data[:-4]))
-                try:
-                    unpacked = msgpack.unpackb(data[:-4], raw=False)
-                    #print("Unpacked data: {}\n".format(unpacked[-6:]))
-                    print("Unpacked data: {}\n".format(unpacked))
-                except:
-                    print(f'ERROR --- msgpackerror\n')
+            #if len(data) > 4 and data[-4:] == b'408A':
+            data = b'\x96' + data.split(b'\x96')[-1]
+            #b, d = calcFirstByte(data[:-4])
+            #print("Received data: {}".format(data))
+            #print("unpacking {}".format(b + d))
+            #print("unpacking {}".format(data[:-4]))
+            try:
+                unpacked = msgpack.unpackb(data[:-4], raw=False)
+                #print("Unpacked data: {}\n".format(unpacked[-6:]))
+                print("Unpacked data: {}\n".format(unpacked))
+            except:
+                print(f'ERROR --- msgpackerror\n')
                 
-                datacount += 1
-                data = b''
-                #sendit = True
+            datacount += 1
+            data = b''
+            #sendit = True
             
             #time.sleep(7)
             
